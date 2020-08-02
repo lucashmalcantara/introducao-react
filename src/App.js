d3fe24efb5c4433aa612cf651c059508f1cdfe3a
Lucas Alcantara <lucashmalcantara@gmail.com>
2020-08-02 14:17:33 -0300

    Criado formulário para preencher a lista.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form'
 
 // Este é um exemplo de Class Component. Class e Simple components podem ser mistrurados.
 class App extends Component {
   state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ],
+    characters: [],
   }
 
   removeCharacter = (index) => {
@@ -34,12 +18,17 @@ class App extends Component {
     })
   }
 
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] })
+  }
+
   render() {
     const { characters } = this.state
 
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     )
   }
